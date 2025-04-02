689396e16e79819d53e4da8193a5f0c53c7ffc35
PiresRuben <165799556+PiresRuben@users.noreply.github.com>
2025-04-02 15:50:46 +0200

    update condition bounce

--- Game/src/bird.cpp
+++ Game/src/bird.cpp
@@ -148,9 +148,9 @@ void Bird::update(const sf::RenderWindow& window, sf::Time delta) {
         velocity.x += -f2 * velocity.x * dt; // Friction horizontale
         velocity.y += g * dt - f2 * velocity.y * dt; // Gravité + friction verticale
 
-        if (rect.getPosition().y + 45 > 610) {
+        if (rect.getPosition().y + 34 > 610) {
             velocity.y =  - velocity.y  * cr;
-            rect.setPosition(rect.getPosition().x, 610 - 45); // Teleporte juste au dessus du sol
+            rect.setPosition(rect.getPosition().x, 610 - 34); // Teleporte juste au dessus du sol
         }
 
         // Verification des collisions avec les bords
